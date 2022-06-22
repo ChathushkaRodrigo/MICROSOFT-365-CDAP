0f169a22a4426219f34d5ddf98afa6b250083987
Chathushka Rodrigo <72795289+ChathushkaRodrigo@users.noreply.github.com>
2022-06-22 18:13:59 +0530

    Updated url

--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,6 @@
 const config = {
   appId: '544352e1-ab72-455c-afad-f2e10ca0f19c',
-  redirectUri: 'http://localhost:3000',
+  redirectUri: 'https://cdap-app-365.herokuapp.com/',
   scopes: [
     'user.read',
     'mailboxsettings.read',
